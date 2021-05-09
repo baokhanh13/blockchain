ee20d5ec428c01da1a0a6a9e30ee9c2f9d7c8d44
baokhanh13 <baokhanh1306nsc@gmail.com>
2021-05-10 01:24:06 +0700

    Add socket

--- src/socket.js
+++ src/socket.js
@@ -1,12 +1,30 @@
-const { Blockchain } = require("./blockchain")
+const { Blockchain, Transaction } = require("./blockchain");
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
 let blockchain = new Blockchain();
 
 module.exports = function (io, socket) {
     console.log('New user connected');
+    socket.on('identity', ({ privateKey }) => {
+        socket.privateKey = privateKey;
+    });
+
     socket.on('disconnect', () => {
         console.log('disconnected...');
     })
 
-    socket.
+    socket.on('mine', (address) => {
+        blockchain.minePendingTransactions(address);
+    });
+
+    socket.on('transfer', ({ fromAddress, toAddress, amount }) => {
+        const tx = new Transaction(fromAddress, toAddress, amount);
+        tx.signTransaction(ec.keyFromPrivate(socket.privateKey));
+        blockchain.addTransaction(tx);
+    });
+
+    socket.on('get-balance', (address) => {
+        socket.emit('balance', blockchain.getBalanceOfAddress(address));
+    });
 }
